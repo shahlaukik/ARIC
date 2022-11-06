d4639c7268fad427d8e63cfa283e743255ecc38f
Laukik Shah <78136178+shahlaukik@users.noreply.github.com>
2022-11-06 06:23:50 +0530

    major restructuring, navbar,hero section and about ARIC sections remade

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 // Add shadow to navigation bar when scrolling
 window.addEventListener("scroll", (e) => {
-    const navPane = document.querySelector(".nav__pane");
+    const navPane = document.querySelector(".navbar");
     if (window.pageYOffset > 0) {
-        navPane.classList.add("nav__shadow");
+        navPane.classList.add("navbarShadow");
     } else {
-        navPane.classList.remove("nav__shadow");
+        navPane.classList.remove("navbarShadow");
     }
 });
+
+// Toggle visibility of navigation menu on mobile
+const navOptions2 = document.querySelector(".navOptions2");
+function toggleNavOptions2() {
+    navOptions2.classList.toggle("navOptions2Visible");
+}
